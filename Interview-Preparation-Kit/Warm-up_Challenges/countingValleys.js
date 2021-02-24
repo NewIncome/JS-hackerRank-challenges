e6c2dcbe63b0911a74645b9f7a62ca62aa422edf
NewIncome <alfredo_cardenas@live.com>
2021-02-24 12:58:03 -0600

    Add updated code, satisfying all TestCases that failed due to TimeOut

--- Interview-Preparation-Kit/Warm-up_Challenges/countingValleys.js
+++ Interview-Preparation-Kit/Warm-up_Challenges/countingValleys.js
@@ -1,15 +1,23 @@
 // How many valleys does he enter and leave
 
 function countingValleys(steps, path) {
-  // Write your code here
   let hills = 0;
   let pathNum = 0;
-  path.split('').forEach(stp => {
-    console.log(pathNum);
-    if (pathNum === 0 && stp === 'D') hills += 1;
-    stp === 'U' ? pathNum += 1 : pathNum -= 1;
-  });
+  for (let i=0; i<=steps; i++) {
+    if (pathNum === 0 && path[i] === 'D') hills += 1;
+    path[i] === 'U' ? pathNum += 1 : pathNum -= 1;
+  }
   return hills;
+
+  // >>>>> Solution returns 'Time limit exceeded' for TC 14-21 <<<<<
+  // let hills = 0;
+  // let pathNum = 0;
+  // path.split('').forEach(stp => {
+  //   console.log(pathNum);
+  //   if (pathNum === 0 && stp === 'D') hills += 1;
+  //   stp === 'U' ? pathNum += 1 : pathNum -= 1;
+  // });
+  // return hills;
 }
 
 //_/\      _
@@ -21,3 +29,9 @@ console.log(countingValleys(8, 'UDDDUDUU'));
 //  \/  \/\/
 console.log(countingValleys(12, 'DDUUDDUDUUUD'));
 
+/*   >>>>> Fastest Loop in JavaScript? <<<<<               vvvvv
+for (let i = arr.length; i > 0; i--) {} <-- for(reverse) :- 1.5ms
+for (let i = 0; i < arr.length; i++) {} <-- for          :- 1.6ms
+arr.forEach(v => v)                     <-- foreach      :- 2.1ms
+for (const v of arr) {}                 <-- for...of     :- 11.7ms
+*/
