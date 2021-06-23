933b8c051ceec28c42d348c2032542633286fba3
NewIncome <alfredo_cardenas@live.com>
2021-06-23 17:31:49 -0500

    Add comment explaining my reasoning. Code 4 3 TC TimeExceeds

--- Interview-Preparation-Kit/Arrays/minSwap-2.js
+++ Interview-Preparation-Kit/Arrays/minSwap-2.js
@@ -1,15 +1,15 @@
 function minimumSwaps(arr) {
+  console.time('minS');
   let swaps = 0;
-  for(let i=0 ; i<arr.length ; i++) {
-    while((arr[i]-1) !== i) {
-      const swVl = arr[i];
-      console.log(arr[i], arr[swVl-1]);
-      arr[i] = arr[swVl-1];
-      arr[swVl-1] = swVl;
-      swaps += 1;
-      console.log(arr);
-    }
+  while((arr[i]-1) !== i) {
+    const swVl = arr[i];
+    console.log(arr[i], arr[swVl-1]);
+    arr[i] = arr[swVl-1];
+    arr[swVl-1] = swVl;
+    swaps += 1;
+    console.log(arr);
   }
+  console.timeEnd('minS');
   return swaps;
 }
 
@@ -27,6 +27,10 @@ console.log(minimumSwaps([4, 3, 1, 2]));
 // console.log('');
 // console.log(minimumSwaps([1, 3, 5, 2, 4, 6, 7]));
 
-/*
-  
+/* Explanation : My Understanding
+  Have to:
+  - loop over the array
+  - checking every time: IF the elm has index = it'sVal - 1
+    - IF Yes: Swap That-Value with the value in the index [it'sVal - 1]
+  - return the Times-Swaped
 */
