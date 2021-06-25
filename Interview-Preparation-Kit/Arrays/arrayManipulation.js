4fc90e4616022dccd4b255e300ed3ba48e19068a
NewIncome <alfredo_cardenas@live.com>
2021-06-25 15:40:55 -0500

    Passed only 6/15 TC

--- Interview-Preparation-Kit/Arrays/arrayManipulation.js
+++ Interview-Preparation-Kit/Arrays/arrayManipulation.js
@@ -1,6 +1,5 @@
 function arrayManipulation(n, queries) {
   // Write your code here
-  //     for (let i=0 ; i<queries.length ; i++) {
   // [ [ 1, 2, 100 ], [ 2, 5, 100 ], [ 3, 4, 100 ] ]
   let maxValArr = new Array(n).fill(0);
   let w = 0, maxVal = 0;
@@ -11,18 +10,15 @@ function arrayManipulation(n, queries) {
     for (let i=a ; i<=b ; i++) indexes.push(i);
 
     maxValArr  = new Array(...maxValArr.map((e,i) => {
-      // if (e+k > maxVal) maxVal = e+k;
-      // return indexes.includes(i+1) ? e+k : e;
       if (indexes.includes(i+1)) {
         if (e+k > maxVal) maxVal = e+k;
         return e+k;
       }
       else return e;
     }));
-    console.log(indexes);
+    // console.log(indexes);
     w += 1;
-    console.log(`[${maxVal}]`);
   }
+  return maxVal;
 }
-
-arrayManipulation(10, [ [ 1, 2, 100 ], [ 2, 5, 100 ], [ 3, 4, 100 ] ]);
+console.log(arrayManipulation(10, [ [ 1, 2, 100 ], [ 2, 5, 100 ], [ 3, 4, 100 ] ]));
