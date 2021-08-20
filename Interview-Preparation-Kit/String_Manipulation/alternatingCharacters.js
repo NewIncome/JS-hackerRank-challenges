c5f1fe292cc58df256649968b8cc2beb6098d7da
Alfredo C <alfredo_cardenas@live.com>
2021-08-20 18:15:52 -0500

    Make and initialize the challenge alternatingCharacters challenge

---
+++ Interview-Preparation-Kit/String_Manipulation/alternatingCharacters.js
@@ -0,0 +1,8 @@
+function alternatingCharacters(s) {
+  // Write your code here
+
+  // return // int: the number of deletions so there are no matching adjacent letters
+}
+
+// T.C.0: 
+console.log(alternatingCharacters([AAAA,BBBBB,ABABABAB,BABABA,AAABBB]));
