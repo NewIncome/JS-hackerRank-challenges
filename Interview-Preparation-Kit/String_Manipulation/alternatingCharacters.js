acf7d23468d6d02053e308dce10de996d90d9701
Alfredo C <alfredo_cardenas@live.com>
2021-08-23 16:26:44 -0500

    Add solution, passed 14/15 TestCases. TC11 with TLE error

--- Interview-Preparation-Kit/String_Manipulation/alternatingCharacters.js
+++ Interview-Preparation-Kit/String_Manipulation/alternatingCharacters.js
@@ -1,8 +1,21 @@
 function alternatingCharacters(s) {
   // Write your code here
-
+  let arrS = s.split('');
+  let cnt = 0;
+  for (let i=0 ; i<(arrS.length-1) ; i++) {
+    // console.log(arrS[i], ',', arrS[i+1], ',', arrS[i-1] === arrS[i]);
+    if (arrS[i] === arrS[i+1]) {
+      arrS.splice((i+1),1);
+      cnt += 1;
+      i = i - 1;
+    }
+  }
+  return cnt;
   // return // int: the number of deletions so there are no matching adjacent letters
 }
 
 // T.C.0: 
-console.log(alternatingCharacters([AAAA,BBBBB,ABABABAB,BABABA,AAABBB]));
+const strings = ['AAAA','BBBBB','ABABABAB','BABABA','AAABBB'];
+for(const string of strings) {
+  console.log(alternatingCharacters(string));
+}
