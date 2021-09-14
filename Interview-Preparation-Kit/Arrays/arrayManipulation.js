a502694fe3f91f267c38b750508209ebfb756d75
Alfredo C <alfredo_cardenas@live.com>
2021-09-13 22:52:21 -0500

    Add modification for code to pass better the tests and test runtime

--- Interview-Preparation-Kit/Arrays/arrayManipulation.js
+++ Interview-Preparation-Kit/Arrays/arrayManipulation.js
@@ -7,9 +7,10 @@ function arrayManipulation(n, queries) {
 
   queries.forEach(query => {
     // console.log(query);
-    valArr[query[0]-1] = query[2];
-    valArr[query[1]-1] = -query[2];
+    valArr[query[0]-1] += query[2];
+    valArr[query[1]] += -query[2];
   });
+  console.log(valArr);
   valArr.forEach(val => {
     cur += val;
     if (max == null) max = val;
@@ -30,10 +31,16 @@ console.log(arrayManipulation(10, [[1,5,3],[4,8,7],[6,9,1]]));
 console.log(arrayManipulation(10, [[2,6,8],[3,5,7],[1,8,1],[5,9,15]]));
 /*       wwhile   BDe
    TC | time(ms) time(ms)
-    0    5.885    0.117
-    1    0.230    0.016
-   14    0.139    0.010
-   15    0.157    0.016
+    0    5.885    0.118
+    1    0.230    0.022
+   14    0.139    0.014
+   15    0.157    0.009
 */
 
 // Solution thanks to BrianDyck's explanation : [3 0 0 7 0 -2 0 0 -7 -1]
+
+/* T.C.0
+  [100 100 -100(+100) 0 -100 -100 0 0 0 0]
+   T.C.1
+  [286 603(-286) 0 -603(+882)] -882
+*/
