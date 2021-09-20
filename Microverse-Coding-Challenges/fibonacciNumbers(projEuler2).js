7ce89643dc14c0cbbe1e6067d84296ebc83d10dc
Alfredo C <alfredo_cardenas@live.com>
2021-09-20 16:23:45 -0500

    Add code solution. Passed all TCs

--- Microverse-Coding-Challenges/fibonacciNumbers(projEuler2).js
+++ Microverse-Coding-Challenges/fibonacciNumbers(projEuler2).js
@@ -1,5 +1,14 @@
 function fib(n) {
-  
+  let fibs = [1n, 2n];
+  let num = 0n;
+  let i = 1;
+  while (num < n) {
+    num = fibs[i-1] + fibs[i];
+    if(num > n) break;
+    fibs.push(num);
+    i++;
+  }
+  console.log(fibs.filter(e => e%2n==0).reduce((a,b) => a+b).toString());
 }
 
 // T.C.example0 : 10
