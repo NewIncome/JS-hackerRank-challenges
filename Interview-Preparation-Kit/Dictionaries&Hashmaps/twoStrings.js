3eed63bd91dd54e19663f434c89716c9ba7b6ed6
Alfredo C <alfredo_cardenas@live.com>
2021-09-24 11:28:19 -0500

    Add solution for two strings challenge. Passed 8/8 TC's

--- Interview-Preparation-Kit/Dictionaries&Hashmaps/twoStrings.js
+++ Interview-Preparation-Kit/Dictionaries&Hashmaps/twoStrings.js
@@ -1,6 +1,10 @@
 // return true if there is at leas one char in common
 function twoStrings(s1, s2) {
   // Write your code here
+  for(let i=0 ; i<s1.length ; i++) {
+    if(s2.includes(s1[i])) return 'YES';
+  }
+  return 'NO';
 }
 
 // T.C.example0 : YES
