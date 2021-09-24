9e900efeb82b5f67b1597cae144a2a3e2bc3de19
Alfredo C <alfredo_cardenas@live.com>
2021-09-24 10:37:14 -0500

    Add default function call and base test cases

---
+++ Interview-Preparation-Kit/Dictionaries&Hashmaps/twoStrings.js
@@ -0,0 +1,9 @@
+// return true if there is at leas one char in common
+function twoStrings(s1, s2) {
+  // Write your code here
+}
+
+// T.C.example0 : YES
+twoStrings('hello','world')
+// T.C.example1 : NO
+twoStrings('hi','world')
