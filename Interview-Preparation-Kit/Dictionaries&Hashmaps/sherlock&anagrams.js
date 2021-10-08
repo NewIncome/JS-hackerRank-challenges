94be17d08e232bf8aa1235f43f2532067bc132d6
Alfredo C <alfredo_cardenas@live.com>
2021-10-08 13:11:11 -0500

    Add file and default func call with base test cases for Sherlock and Anagram coding challenge

---
+++ Interview-Preparation-Kit/Dictionaries&Hashmaps/sherlock&anagrams.js
@@ -0,0 +1,11 @@
+// return the number of unordered anagrammatic pairs of substrings
+function sherlockAndAnagrams(s) {
+    // Write your code here
+
+}
+
+// T.C.example1 : 4
+console.log(sherlockAndAnagrams('abba'));
+// T.C.example2 : 0
+console.log(sherlockAndAnagrams('abcd'));
+
