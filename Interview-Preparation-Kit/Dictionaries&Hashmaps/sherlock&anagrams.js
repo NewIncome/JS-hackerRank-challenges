35ee91457b521b821d465a97a501342ca12c33c2
Alfredo C <alfredo_cardenas@live.com>
2021-10-12 14:20:05 -0500

    Add calls and arguments for T.C's 1 and 2

--- Interview-Preparation-Kit/Dictionaries&Hashmaps/sherlock&anagrams.js
+++ Interview-Preparation-Kit/Dictionaries&Hashmaps/sherlock&anagrams.js
@@ -12,16 +12,22 @@ function sherlockAndAnagrams(s) {
       let curr = s.slice(i,i+j);
       for(let k=i+1 ; k<=(s.length-j) ; k++) { // end
         let anagramMaybe = s.slice(k,k+j).split('').reverse().join('');
-        console.log(`ijk:${i}${j}${k}, curr:${curr}, rruc:${anagramMaybe} ; s:${s} ${s.length}`);
+        // console.log(`ijk:${i}${j}${k}, curr:${curr}, rruc:${anagramMaybe} ; s:${s} ${s.length}`);
         if(curr == anagramMaybe) anagramCnt += 1;
       }
     }
   }
-  console.log('');
+  // console.log('');
   return anagramCnt;
 }
 
-// T.C.example1 : 4
+// T.C.0-example1 : 4
 console.log(sherlockAndAnagrams('abba'));
-// T.C.example2 : 0
+// T.C.0-example2 : 0
 console.log(sherlockAndAnagrams('abcd'));
+// T.C.1-1 : 3
+console.log(sherlockAndAnagrams('ifailuhkqq'));
+// T.C.1-2 : 10
+console.log(sherlockAndAnagrams('kkkk'));
+// T.C.2 : 5
+console.log(sherlockAndAnagrams('cdcd'));
