fdc4b336d3978028b2dcf1d79d74dd540547814d
Alfredo C <alfredo_cardenas@live.com>
2021-10-14 12:25:35 -0500

    Add base function call and test cases for count triplets challenge

---
+++ Interview-Preparation-Kit/Dictionaries&Hashmaps/countTriplets.js
@@ -0,0 +1,11 @@
+// Complete the countTriplets function below.
+function countTriplets(arr, r) {
+}
+
+// T.C.0: 2
+console.log(countTriplets([1,2,2,4], 2));
+// T.C.1: 6
+console.log(countTriplets([1,3,9,9,27,81], 3));
+// T.C.2: 4
+console.log(countTriplets([1,5,5,25,125], 5));
+
