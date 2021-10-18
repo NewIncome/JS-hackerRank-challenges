7e2b0c4dec1b3487983b68e712ebd2fc9f56dac8
Alfredo C <alfredo_cardenas@live.com>
2021-10-18 10:51:21 -0500

    Write base TC's and basic function call for luck balance challenge

---
+++ Interview-Preparation-Kit/Greedy_Algorithms/luckBalance.js
@@ -0,0 +1,10 @@
+function luckBalance(k, contests) {
+}
+
+// T.C.0: 29
+console.log(luckBalance(3, [[5,1],[2,1],[1,1],[8,1],[10,0],[5,0]]));
+// T.C.1: 42
+console.log(luckBalance(5, [[13,1],[10,1],[9,1],[8,1],[13,1],[12,1],[18,1],[13,1]]));
+// T.C.2: 21
+console.log(luckBalance(2, [[5,1],[4,0],[6,1],[2,1],[8,0]]));
+
